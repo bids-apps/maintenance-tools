8237091c80a14a3160e676916adae3239e980954
Remi Gau <remi_gau@hotmail.com>
2023-01-04 19:09:02 +0100

    add push script

--- circleci/build_docker.sh
+++ circleci/build_docker.sh
@@ -10,11 +10,19 @@
 #     - ~/docker
 #     - ~/data
 # 
+# USAGE:
+# 
+# - run:
+#     name: build docker image
+#     command: |
+#       wget https://raw.githubusercontent.com/bids-apps/maintenance-tools/main/circleci/build_docker.sh
+#       bash build_docker.sh  
+#   
 
 if [[ -e ${HOME}/docker/image.tar ]]; then
-    docker load -i ${HOME}/docker/image.tar
+    docker load -i "${HOME}/docker/image.tar"
 fi
 git describe --tags --always > version
-docker build -t bids/${CIRCLE_PROJECT_REPONAME,,} .
-mkdir -p ${HOME}/docker
-docker save "bids/${CIRCLE_PROJECT_REPONAME,,}" > ${HOME}/docker/image.tar
+docker build -t "bids/${CIRCLE_PROJECT_REPONAME,,}" .
+mkdir -p "${HOME}/docker"
+docker save "bids/${CIRCLE_PROJECT_REPONAME,,}" > "${HOME}/docker/image.tar"
---
+++ push_docker.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+# used to push the Docker image for a project in circle CI
+# 
+# USAGE:
+# 
+# - run:
+#     name: push docker image
+#     command: |
+#       wget https://raw.githubusercontent.com/bids-apps/maintenance-tools/main/circleci/push_docker.sh
+#       bash push_docker.sh  
+#  
+
+if [[ -n "$DOCKER_PASS" ]]; then
+
+    # make sure we have a lowercase repo name
+    repo_name=$(echo "${CIRCLE_PROJECT_REPONAME}" | tr '[:upper:]' '[:lower:]')
+
+    echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
+
+    echo "Pushing to DockerHub bids/${repo_name}:unstable"
+    docker tag "bids/${repo_name}" "bids/${repo_name}:unstable"
+    docker push "bids/${repo_name}:unstable"
+
+    if [[ -n "${CIRCLE_TAG}" ]]; then
+        echo "Pushing to DockerHub bids/${repo_name}:${CIRCLE_TAG}"
+        docker push "bids/${repo_name}:latest"
+        docker tag "bids/${repo_name}" "bids/${repo_name}:${CIRCLE_TAG}"
+        docker push "bids/${repo_name}:${CIRCLE_TAG}"
+    fi
+
+else
+    echo "No DOCKER_PASS, skipping push to DockerHub"
+
+fi

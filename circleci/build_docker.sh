9119cc218a270d554242ab771db59ce06b6495ca
Remi Gau <remi_gau@hotmail.com>
2023-01-04 19:40:34 +0100

    add precommit

--- circleci/build_docker.sh
+++ circleci/build_docker.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
 # used to build the Docker image for a project in circle CI
-# 
+#
 #  assumes that the docker has been cached in ${HOME}/docker/image.tar
-# 
+#
 # - save_cache:
 #     key: my_cache
 #     paths:
 #     - ~/docker
 #     - ~/data
-# 
+#
 
-if [[ -e ${HOME}/docker/image.tar ]]; then
-    docker load -i ${HOME}/docker/image.tar
+if [[ -e "${HOME}/docker/image.tar" ]]; then
+    docker load -i "${HOME}/docker/image.tar"
 fi
 git describe --tags --always > version
-docker build -t bids/${CIRCLE_PROJECT_REPONAME,,} .
-mkdir -p ${HOME}/docker
-docker save "bids/${CIRCLE_PROJECT_REPONAME,,}" > ${HOME}/docker/image.tar
+docker build -t "bids/${CIRCLE_PROJECT_REPONAME,,}" .
+mkdir -p "${HOME}/docker"
+docker save "bids/${CIRCLE_PROJECT_REPONAME,,}" > "${HOME}/docker/image.tar"
